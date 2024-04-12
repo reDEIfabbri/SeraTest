615eeeea5e2bfb816338767cc94a03c3600b8817
reDEIfabbri <kmisi.mit.ai2@gmail.com>
2024-04-12 15:09:56 +0200

    working on the goddamn gallery
    Signed-off-by: reDEIfabbri <kmisi.mit.ai2@gmail.com>

---
+++ serving.py
@@ -0,0 +1,10 @@
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+http.server.HTTPServer("", BaseHTTPRequestHandler)
+
+
+class http.server.BaseHTTPRequestHandler()
