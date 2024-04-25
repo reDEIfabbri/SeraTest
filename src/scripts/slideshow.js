cac8ddc93c213d3f48b30d330d2c5a66b7811925
reDEIfabbri <kmisi.mit.ai2@gmail.com>
2024-04-25 21:06:52 +0200

    Differential images, Bitches!

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -1,10 +1,10 @@
 let pn = 1;
 const slide = document.getElementById("slides");
-
+/*
 setInterval(function () {
     slides.src = "/src/pictures/plakat" + pn + ".png";
     num()
-}, 5000);
+}, 5000);*/
 
 function num() {
     pn++;
@@ -14,9 +14,33 @@ function num() {
 
 }
 
-function slideShow(id) {
-    setInterval(function () {
-        id.src = "/src/pictures/plakat" + pn + ".png";
-        num();
-    }, 5000)
+let boolPlay = 1;
+
+let nIntervID;
+
+function changeSlide(id) {
+    id.src = "/src/pictures/plakat" + pn + ".png";
+    num();
+}
+
+function startSlideShow(id) {
+    if (!nIntervID) {
+        nIntervID = setInterval(changeSlide, 5000, id);
+    }
+}
+function stopSlideShow(id) {
+    clearInterval(nIntervID);
+    nIntervID = null;
+}
+
+function btnSlide(id) {
+    alert(id);
+    if(boolPlay == 1) {
+        startSlideShow(id);
+        boolPlay = 0;
+    }
+    else {
+        stopSlideShow(id);
+        boolPlay = 1;
+    }
 }
