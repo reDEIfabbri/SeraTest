90319b90293eec9a67875442589563173c206f5c
reDEIfabbri <kmisi.mit.ai2@gmail.com>
2025-03-05 14:27:16 +0100

    trollok voltak!4!!
    Signed-off-by: reDEIfabbri <kmisi.mit.ai2@gmail.com>

--- src/scripts/db-nav.js
+++ src/scripts/db-nav.js
@@ -12,7 +12,7 @@ for (let i = 0; i < listOfDB.length; i++) {
 
 
 function getCurrentIndex() {
-    for (let i = 0; i < (listOfYears.length - 1); i++) {
+    for (let i = 0; i < (listOfYears.length); i++) {
         if (listOfYears[i].id === "active-db") {
             return i;
         }
@@ -28,30 +28,33 @@ alert("Your test data is: " + listOfTextTest.length);
 function setCurrentIndex(currentIndex, nextIndex) {
     listOfYears[currentIndex].id = "";
     listOfYears[nextIndex].id = "active-db";
+    console.log("Setting the index to: " + nextIndex);
 }
 
 function setPath(nextIndex) {
     path = root + listOfYears[nextIndex].dataset.year + "/";
+    console.log("Setting the year to: " + listOfYears[nextIndex].dataset.year);
 }
 
 function changeYearTo(nextIndex) {
     setCurrentIndex(getCurrentIndex(), nextIndex);
     setPath(nextIndex);
     console.log(path);
-    for (let i = 0; i < (listOfPicks.length - 1); i++) {
+    console.log("Number of changing pictures: " + listOfPicks.length);
+    for (let i = 0; i < (listOfPicks.length); i++) {
         listOfPicks[i].src = path + listOfPicks[i].dataset.name;
         console.log(listOfPicks[i].src);
     }
-
-    console.log("You've clicked: " + nextIndex);
-    console.log(listOfPicks[2].src);
-
+    console.log("Number of available descriptions: " + listOfTextTest.length);
+    listOfTextTest.forEach(member => {
+        member.getElementsByClassName(listOfYears[nextIndex].dataset.year);
+    });
 }
 
 function rolfcopter() {
     alert("Oarr noarr!");
     alert("You've just clicked the wrong button! Now the ROLF Copter is coming to catch you!");
-    setPath(listOfYears.length - 1)
+    setPath(listOfYears.length - 1);
     listOfPicks[0].src = path + "Roflcopter.gif";
     console.log(listOfPicks[0].src);
     document.getElementsByClassName("DB").display = "none";
