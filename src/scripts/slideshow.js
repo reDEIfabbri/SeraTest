ca73d1a4e13154ab4b09e7427492e9651bc19f32
reDEIfabbri <kmisi.mit.ai2@gmail.com>
2026-06-11 18:21:01 +0200

    tamogatok update 2
    Signed-off-by: reDEIfabbri <kmisi.mit.ai2@gmail.com>

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -38,3 +38,46 @@ function startSlideShowCal() {
 }
 
 startSlideShowCal();
+
+/* Gallery Slideshow (used in Tamogatok.html) */
+let slideIndexTm = 1;
+
+// Only auto-run if the elements exist
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementsByClassName("tm-demo").length > 0) {
+        showSlidesTm(slideIndexTm);
+    }
+});
+
+function plusSlidesTm(n) {
+    showSlidesTm(slideIndexTm += n);
+}
+
+function currentSlideTm(n) {
+    showSlidesTm(slideIndexTm = n);
+}
+
+function showSlidesTm(n) {
+    let i;
+    let slides = document.getElementsByClassName("tm-mySlides");
+    let dots = document.getElementsByClassName("tm-demo");
+    let captionText = document.getElementById("tm-caption");
+    if (!slides.length) return;
+    if (n > slides.length) {
+        slideIndexTm = 1
+    }
+    if (n < 1) {
+        slideIndexTm = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" tm-active", "");
+    }
+    slides[slideIndexTm - 1].style.display = "block";
+    dots[slideIndexTm - 1].className += " tm-active";
+    if (captionText) {
+        captionText.innerHTML = dots[slideIndexTm - 1].alt;
+    }
+}
